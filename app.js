3f812e250dba8b0b77b3e92ddd1b119284308376
Ben Liu <ben.liu@hopebaytech.com>
2016-01-19 15:19:29 +0800

    add some new packages

--- app.js
+++ app.js
@@ -1,33 +1,74 @@
 'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var passport = require('passport');
 var mongoose = require('mongoose');
+var flash = require("connect-flash");
+var helmet = require('helmet');
+var csrf = require('csurf');
 
+var config = require('./config');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// create express app
 var app = express();
+
+// keep reference to config
+app.config = config;
+
+// setup mongoose
+app.db = mongoose.createConnection(require('./database').uri);
+app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
+app.db.once('open', function(){
+  //we have data store
+});
+
+// config data models
+require('./models')(app, mongoose);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(config.cryptoKey));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/login', passport);
+// required for passport
+app.use(session({
+  secret: config.cryptoKey
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+// app.use(csrf({ cookie: { signed: true } }));
+// helmet(app);
+
+// // response locals
+// app.use(function(req, res, next){
+//   res.cookie('_csrfToken', req.csrfToken());
+//   res.locals.user = {};
+//   res.locals.user.defaultReturnUrl = req.user.defaultReturnUrl();
+//   res.locals.user.username = req.user.username;
+//   next();
+// });
+
+// setup passport
+require('./passport')(app, passport);
+
+// setup routes
+require('./routes')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
