6236fa9e93a9116c22386ed05e3fbdbfa1d31d39
ben196888 <ben196888@gmail.com>
2016-01-20 00:46:22 +0800

    update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 exports.oauth = {
 	google:
 	{
-		key: '285720300958-hi63cc599a9ur3e8p11k6i3dafv8udme.apps.googleusercontent.com',
-		secret: 'DtlHfCYB__6NQGMkDptd8Y_X'
+		key: process.env.googleClientID,
+		secret: process.env.googleSecret
 	}
 };
 
